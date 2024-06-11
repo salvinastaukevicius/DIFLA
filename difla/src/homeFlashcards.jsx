45fcd6484d9da591fbc20f3347e0d4d02041d9b1
Salvinas <i481136@fhict.nl>
2024-06-11 23:30:15 +0200

    flashcards home

--- difla/src/homeFlashcards.jsx
+++ difla/src/homeFlashcards.jsx
@@ -1,7 +1,7 @@
 // src/MyComponent.js
 import React from 'react';
 import arrowOrangeRight from './arrowOrangeRight.svg';
-import blueHome from './homeBackground.png'
+import speakerOrange from './orangeSpeaker.svg';
 import iceBlock from './ice.png';
 import penguinWave from './penguin_wave.png'
 import iconSpeaker from './sound.svg'
@@ -10,13 +10,45 @@ import './homeFlashcards.css'
 const HomeFlashcards = () => {
   return (
     <div>
+
     <p id='titleFlashcards'>Flashcards</p>
+
     <section id='buttonSeeAll'>
         <p id='buttonSeeAllText'>Bekijk alles</p>
         <img id='arrowOrangeRight' src={arrowOrangeRight} />
     </section>
-    <section>
 
+    <section id='cardSection'>
+    <div id='card'>
+    <p id='cardLetter'>a</p>
+    <img id='speakerIcon' src={speakerOrange}/>
+    <button id='buttonStart'>Starten</button>
+    </div>
+    <div id='card'>
+    <p id='cardLetter'>e</p>
+    <img id='speakerIcon' src={speakerOrange}/>
+    <button id='buttonStart'>Starten</button>
+    </div>
+    <div id='card'>
+    <p id='cardLetter'>i</p>
+    <img id='speakerIcon' src={speakerOrange}/>
+    <button id='buttonStart'>Starten</button>
+    </div>
+    <div id='card'>
+    <p id='cardLetter'>aa</p>
+    <img id='speakerIcon' src={speakerOrange}/>
+    <button id='buttonStart'>Starten</button>
+    </div>
+    <div id='card'>
+    <p id='cardLetter'>ee</p>
+    <img id='speakerIcon' src={speakerOrange}/>
+    <button id='buttonStart'>Starten</button>
+    </div>
+    <div id='card'>
+    <p id='cardLetter'>uu</p>
+    <img id='speakerIcon' src={speakerOrange}/>
+    <button id='buttonStart'>Starten</button>
+    </div>
     </section>
 
     </div>
