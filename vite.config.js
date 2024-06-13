fd8204c08356599129d4caf6fcd854ae94eb7bb7
Salvinas <i481136@fhict.nl>
2024-06-13 13:30:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DIFLA/', // Replace <repository-name> with your repository name
+  base: '/dist/', // Replace <repository-name> with your repository name
 });
